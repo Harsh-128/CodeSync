5c279dd0e03bd885ef24d566ffbcb6b59187f755
Harsh-128 <harshee2000@gmail.com>
2026-08-08 15:04:56 +0530

    Fix configurable Judge0 URL

--- server/services/judge0.js
+++ server/services/judge0.js
@@ -1,21 +1,20 @@
 const axios = require("axios");
 
-// Judge0 URL
-const JUDGE0_URL = "http://host.docker.internal:2358";
+const JUDGE0_URL =
+    process.env.JUDGE0_URL || "http://host.docker.internal:2358";
 
 const submitCode = async (language_id, source_code, stdin = "") => {
     try {
         const response = await axios.post(
-    `${JUDGE0_URL}/submissions?base64_encoded=false&wait=true`,
-    {
-        language_id,
-        source_code,
-        stdin,
-    }
-);
+            `${JUDGE0_URL}/submissions?base64_encoded=false&wait=true`,
+            {
+                language_id,
+                source_code,
+                stdin,
+            }
+        );
 
         return response.data;
-
     } catch (error) {
         console.error("Judge0 Error:", error.message);
         throw error;
